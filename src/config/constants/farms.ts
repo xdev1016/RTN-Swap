59e9321b64ffc56446351ce91d70691fb97d6f10
CRYPTODEV <xiangchu1016@gmail.com>
2022-01-14 22:41:22 -0500

    first commit

---
+++ src/config/constants/farms.ts
@@ -0,0 +1,41 @@
+import tokens from './tokens'
+import { FarmConfig } from './types'
+
+const farms: FarmConfig[] = [
+  /**
+   * These 3 farms (PID 0, 251, 252) should always be at the top of the file.
+   */
+  {
+    pid: 0,
+    lpSymbol: 'CAKE',
+    lpAddresses: {
+      97: '0x9C21123D94b93361a29B2C2EFB3d5CD8B17e0A9e',
+      56: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
+    },
+    token: tokens.syrup,
+    quoteToken: tokens.wbnb,
+  },
+  {
+    pid: 251,
+    lpSymbol: 'CAKE-BNB LP',
+    lpAddresses: {
+      97: '0x3ed8936cAFDF85cfDBa29Fbe5940A5b0524824F4',
+      56: '0x0eD7e52944161450477ee417DE9Cd3a859b14fD0',
+    },
+    token: tokens.cake,
+    quoteToken: tokens.wbnb,
+  },
+  {
+    pid: 252,
+    lpSymbol: 'BUSD-BNB LP',
+    lpAddresses: {
+      97: '',
+      56: '0x58F876857a02D6762E0101bb5C46A8c1ED44Dc16',
+    },
+    token: tokens.busd,
+    quoteToken: tokens.wbnb,
+  },
+  
+]
+
+export default farms
